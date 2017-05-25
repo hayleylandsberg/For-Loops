910cf8d1e446544d8be949b9bcc494d8c331664e
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-05-25 13:24:39 -0500

    adding files

---
+++ for-loops.js
@@ -0,0 +1,19 @@
+for (var i = 5; i <= 120; i +=10) {
+	console.log(i);
+}
+
+for (var i = 4096; i >= 1; i /=2) {
+	console.log(i);
+}
+
+var americanPresidents = [
+	'George Washington',
+	'John Adams',
+	'Thomas Jefferson',
+	'James Madison',
+	'James Monroe',
+];
+
+for (var i = 0; i < americanPresidents.length; i++) {
+	console.log(americanPresidents[i]);
+}
